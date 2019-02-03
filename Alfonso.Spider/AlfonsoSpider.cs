cc4f1e544833ac9db9aab0927feb6a8349587c87
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-03 15:27:21 +0300

    BaiduSearchEntry added

---
+++ Alfonso.Spider/AlfonsoSpider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotnetSpider.Core;
+using DotnetSpider.Extension;
+
+namespace Alfonso.Spider
+{
+    [TaskName("AlfonsoSpider")]
+    internal class AlfonsoSpider : EntitySpider
+    {
+        public AlfonsoSpider() : base("AlfonsoSpider")
+        {
+        }
+
+        protected override void OnInit(params string[] arguments)
+        {
+            var word = "araba";
+
+            AddRequest(string.Format("http://news.baidu.com/ns?word={0}&tn=news&from=news&cl=2&pn=0&rn=20&ct=1", word), new Dictionary<string, dynamic> { { "Keyword", word } });
+            AddEntityType<BaiduSearchEntry>();
+        }
+    }
+}
---
+++ Alfonso.Spider/BaiduSearchEntry.cs
@@ -0,0 +1,51 @@
+﻿using DotnetSpider.Extension.Model;
+using DotnetSpider.Extraction;
+using DotnetSpider.Extraction.Model;
+using DotnetSpider.Extraction.Model.Attribute;
+using DotnetSpider.Extraction.Model.Formatter;
+
+namespace Alfonso.Spider
+{
+    [Schema("baidu", "baidu_search_entity_model")]
+    [Entity(Expression = ".//div[@class='result']", Type = SelectorType.XPath)]
+    internal class BaiduSearchEntry : BaseEntity
+    {
+        [Column]
+        [Field(Expression = "Keyword", Type = SelectorType.Enviroment)]
+        public string Keyword { get; set; }
+
+        [Column]
+        [Field(Expression = ".//h3[@class='c-title']/a")]
+        [ReplaceFormatter(NewValue = "", OldValue = "<em>")]
+        [ReplaceFormatter(NewValue = "", OldValue = "</em>")]
+        public string Title { get; set; }
+
+        [Column]
+        [Field(Expression = ".//h3[@class='c-title']/a/@href")]
+        public string Url { get; set; }
+
+        [Column]
+        [Field(Expression = ".//div/p[@class='c-author']/text()")]
+        [ReplaceFormatter(NewValue = "-", OldValue = "&nbsp;")]
+        public string Website { get; set; }
+
+        [Column]
+        [Field(Expression = ".//div/span/a[@class='c-cache']/@href")]
+        public string Snapshot { get; set; }
+
+        [Column]
+        [Field(Expression = ".//div[@class='c-summary c-row ']", Option = FieldOptions.InnerText)]
+        [ReplaceFormatter(NewValue = "", OldValue = "<em>")]
+        [ReplaceFormatter(NewValue = "", OldValue = "</em>")]
+        [ReplaceFormatter(NewValue = " ", OldValue = "&nbsp;")]
+        public string Details { get; set; }
+
+        [Column(0)]
+        [Field(Expression = ".", Option = FieldOptions.InnerText)]
+        [ReplaceFormatter(NewValue = "", OldValue = "<em>")]
+        [ReplaceFormatter(NewValue = "", OldValue = "</em>")]
+        [ReplaceFormatter(NewValue = " ", OldValue = "&nbsp;")]
+        public string PlainText { get; set; }
+
+    }
+}
